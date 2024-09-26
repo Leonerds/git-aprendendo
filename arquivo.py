66282daa24f202052b583cc080f7ce1f60e34246
Leonardo Camille <leox_souza@hotmail.com>
2024-09-25 21:20:25 -0300

    Alterado o conteúdo do arquivo

---
+++ arquivo.py
@@ -0,0 +1,13 @@
+git init
+git add arquivo.py
+git config --global user.name "Francisco"
+git config --global user.mail "francisco@gmail.com"
+
+git status
+git commit -m "Adicionado arquivo Principal"
+git status
+git branch -M main
+
+--Criar o repo no GitHub
+git remote add origin <link>
+git push origin main
